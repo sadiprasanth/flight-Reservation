60f8dc345e2e6f34a8875b8efdac536bc56aacf3
SADIII <142563133+sadiprasanth@users.noreply.github.com>
2025-04-15 21:17:55 +0530

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCGJxku5HfYG1-gVAjkIzCW9VCZsJvTve8",
+  authDomain: "fir-56565.firebaseapp.com",
+  projectId: "fir-56565",
+  storageBucket: "fir-56565.appspot.com", // Fixed from firebasestorage.app
+  messagingSenderId: "321628501000",
+  appId: "1:321628501000:web:78ec97e05d10e6529fcdbd"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
